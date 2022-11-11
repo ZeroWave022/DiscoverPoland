9ec25ca86a410bb3d80926aaa21934509d9b133f
ZeroWave_ <36341766+ZeroWave022@users.noreply.github.com>
2022-11-11 11:23:15 +0100

    feat(FullscreenVideo): Make video use all screen space

--- components/FullscreenVideo/FullscreenVideo.js
+++ components/FullscreenVideo/FullscreenVideo.js
@@ -1,22 +1,27 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Head from 'next/head';
-import Navbar from "../Navbar/navbar.js";
 
 import styles from "./FullscreenVideo.module.css";
 
 function FullscreenVideo({ src }) {
     const [height, setHeight] = React.useState(0);
 
+    function updateHeight() {
+        let offsetFromTop = window.innerHeight - ref.current.offsetTop;
+
+        setHeight(offsetFromTop);
+    }
+
     React.useEffect(() => {
-        setHeight(window.innerHeight - ref.current.offsetTop);
+        window.addEventListener("resize", updateHeight);
+        updateHeight();
     }, []);
     
     let ref = React.useRef(null);
     
     return (
-        <div className={styles.container}>
-            <video ref={ref} src={src} autoPlay muted></video>
+        <div ref={ref} className={styles.container}>
+            <video  src={src} autoPlay muted style={{ "height": height }}></video>
             <p className={styles.videoInfo}>All rights belong to their respective owners. <br />Originally filmed by <a href="https://www.youtube.com/c/DKVideoPL/" target="_blank" rel="noreferrer">DK Video</a>, featured <a href="https://www.youtube.com/watch?v=ifVVc8xTas0" target="_blank" rel="noreferrer">here</a>.</p>
         </div>
     );
