728268a4f6183d9bf823c44d49ee8b1f50b37d76
ZeroWave022 <36341766+ZeroWave022@users.noreply.github.com>
2022-11-15 22:44:13 +0100

    feat: Add splash screen on page change

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,42 @@
-import '../styles/globals.css'
+import React from 'react';
+import { useRouter } from 'next/router';
+import '../styles/globals.css';
+
+import Title from '../components/Title/Title.js';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+    const [loading, setLoading] = React.useState(false);
+    const router = useRouter();
+
+    React.useEffect(() => {
+        const handleLoading = () => {
+            setLoading(true);
+        };
+        const handleFinished = () => {
+            // Add a short delay to avoid flashes when changing page.
+            setTimeout(() => {
+                setLoading(false);
+            }, 1000);
+        };
+
+        router.events.on('routeChangeStart', handleLoading);
+        router.events.on('routeChangeComplete', handleFinished);
+        router.events.on('routeChangeError', handleFinished);
+    }, [router]);
+
+    if (loading) {
+        return (
+        <div className="loader">
+            <div className="discoverTitle">
+                <Title size="large" font="SaturdayBright">Discover</Title>
+                <Title size="large" font="SaturdayBright">Poland</Title>
+            </div>
+            <Title size="large">Loading...</Title>
+        </div>
+    )
+    } else {
+        return <Component {...pageProps} />;
+    }
 }
 
-export default MyApp
+export default MyApp;
