39a8def6567b17b9f5a159de535002277133580b
ZeroWave_ <36341766+ZeroWave022@users.noreply.github.com>
2022-11-18 10:05:27 +0100

    feat: Add template pages for cities

---
+++ pages/cities/poznan.js
@@ -0,0 +1,36 @@
+import Layout from "@components/Layout/Layout.js";
+import Title from "@components/Title/Title.js";
+
+import getNavbarLinks from "@lib/getNavbarLinks.js";
+
+const pageConfig = {
+    metadata: {
+        title: "Discover Poland: Cities",
+        description: "Discover Poland: Cities",
+    },
+    navbar: {
+        active: 1,
+    },
+};
+
+export async function getStaticProps() {
+    let navbarLinks = await getNavbarLinks();
+
+    return {
+        props: {
+            metadata: pageConfig.metadata,
+            navbar: {
+                links: navbarLinks,
+                active: pageConfig.navbar.active,
+            },
+        },
+    };
+}
+
+export default function Poznan({ metadata, navbar }) {
+    return (
+        <Layout metadata={metadata} navbar={navbar}>
+            <Title size="large">Poznań</Title>
+        </Layout>
+    );
+}
