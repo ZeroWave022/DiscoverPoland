16cf7a3fd64902567f4758ddacc91411ca060627
ZeroWave_ <36341766+ZeroWave022@users.noreply.github.com>
2022-11-18 12:36:34 +0100

    feat(Footer): Add icons and update design

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -5,33 +5,37 @@ import Title from "@components/Title/Title.js";
 import TextLink from "@components/TextLink/TextLink.js";
 
 import styles from "./Footer.module.css";
+import Link from "next/link";
 
 function Footer() {
     return (
         <div className={styles.container}>
             <div className={styles.general}>
                 <h1>Discover Poland</h1>
-                <TextLink href="#">Cities</TextLink>
-                <TextLink href="#">Blog</TextLink>
-                <TextLink href="/blog">About</TextLink>
+                <TextLink href="/cities">Cities</TextLink>
+                <TextLink href="/blog">Blog</TextLink>
+                <TextLink href="/about">About</TextLink>
             </div>
-            <div className={styles.socials}>
+            <div>
                 <Title size="large">Social Media</Title>
-                <a href="">
-                <Image src="/vercel.svg" width={50} height={50} alt=""/>
-                </a>
+                <div className={styles.socials}>
+                    <Link href="https://facebook.com/" target="_blank">
+                        <Image src="/images/icons/facebook.png" width={50} height={50} alt=""/>
+                    </Link>
 
-                <a href="">
-                <Image src="/vercel.svg" width={50} height={50} alt=""/>
-                </a>
+                    <Link href="https://instagram.com/" target="_blank">
+                        <Image src="/images/icons/instagram.png" width={50} height={50} alt=""/>
+                    </Link>
 
-                <a href="">
-                <Image src="/vercel.svg" width={50} height={50} alt=""/>
-                </a>
+                    <Link href="https://twitter.com/" target="_blank">
+                        <Image src="/images/icons/twitter.png" width={50} height={50} alt=""/>
+                    </Link>
 
-                <a href="">
-                <Image src="/vercel.svg" width={50} height={50} alt=""/>
-                </a>
+                    <Link href="https://youtube.com/" target="_blank">
+                        <Image src="/images/icons/youtube.png" width={50} height={50} alt=""/>
+                    </Link>
+                </div>
+                
             </div>
             <div className={styles.projectInfo}>
                 <Title size="large">About this project</Title>
