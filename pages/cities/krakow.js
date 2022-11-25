dd79b530f5e3506fd62b2c0e2b1dec765af461bb
ZeroWave_ <36341766+ZeroWave022@users.noreply.github.com>
2022-11-25 13:04:30 +0100

    feat: Use new components on city page

--- pages/cities/krakow.js
+++ pages/cities/krakow.js
@@ -1,7 +1,14 @@
+import Image from "next/image";
+
 import Layout from "@components/Layout/Layout.js";
 import Title from "@components/Title/Title.js";
+import LandmarkInfo from "@components/LandmarkInfo/LandmarkInfo.js";
+import FullscreenImage from "@components/FullscreenImage/FullscreenImage.js";
 
 import getNavbarLinks from "@lib/getNavbarLinks.js";
+import { krakow } from "@lib/getThumbnails.js";
+
+import krakow2 from "../../public/images/originals/krakow.jpg";
 
 const pageConfig = {
     metadata: {
@@ -31,6 +38,16 @@ export default function Krakow({ metadata, navbar }) {
     return (
         <Layout metadata={metadata} navbar={navbar}>
             <Title size="large">Kraków</Title>
+            <LandmarkInfo>
+                <div>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis nec ligula ut lectus sodales pellentesque a sit amet lectus. Cras vestibulum imperdiet sodales. Quisque non sem mi. Vestibulum ac accumsan tortor, vel venenatis ipsum. Morbi egestas, augue sed suscipit iaculis, neque magna luctus erat, et vehicula velit nibh vel libero. Nulla facilisi. Sed sodales dolor velit, sit amet cursus lacus placerat ac. Quisque pellentesque, ligula sit amet interdum accumsan, dui augue porttitor elit, vel tincidunt est sapien vel justo. Sed et odio malesuada, pharetra urna malesuada, dignissim libero. Integer porttitor pharetra mauris. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Sed lacinia id diam id condimentum. </p>
+
+                    <p>Aenean luctus, leo ut mollis tincidunt, est libero malesuada mauris, eget facilisis elit quam in lacus. Maecenas finibus, erat a varius sagittis, diam velit egestas orci, vel posuere nunc dolor vitae velit. In fermentum quam maximus risus laoreet, vitae commodo dui auctor. In sit amet auctor magna. Donec et augue in ipsum placerat dignissim. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam elementum, orci vitae vehicula condimentum, nibh neque fringilla quam, quis varius lectus metus at lectus. In vel felis et dolor convallis efficitur iaculis non lacus.</p>
+                    
+                </div>
+                <Image src={krakow} alt="..." />
+            </LandmarkInfo>
+            <FullscreenImage src={krakow2} alt="Wawel Royal Castle" text="Wawel Royal Castle" />
         </Layout>
     );
 }
