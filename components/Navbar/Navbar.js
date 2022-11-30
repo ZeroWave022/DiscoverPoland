6dc0259bffbcc7249f4b9340e27a81d5b703dc4e
ZeroWave022 <36341766+ZeroWave022@users.noreply.github.com>
2022-11-30 22:32:24 +0100

    feat(Navbar): Redesign and make it responsive

--- components/Navbar/Navbar.js
+++ components/Navbar/Navbar.js
@@ -1,7 +1,10 @@
 import React from "react";
 import Link from "next/link";
+import Image from "next/image";
 
 import styles from "./Navbar.module.css";
+
+import MenuImage from "@public/images/icons/menu.png";
 
 /**
  * @param {Object} props
@@ -10,7 +13,14 @@ import styles from "./Navbar.module.css";
  * @returns
  */
 function Navbar({ config }) {
-    let activeClass = (index) => index == config.active ? styles.active : null;
+    const [mobileShown, showMobile] = React.useState(false);
+    
+    let activeClass = (index) => index == config.active ? `${styles.navLink} ${styles.active}` : styles.navLink;
+
+    function toggleMobileMenu(event) {
+        event.preventDefault();
+        showMobile(!mobileShown);
+    }
     
     let navHome = (
         <Link href="/" className={activeClass(0)} key={0}>
@@ -19,17 +29,25 @@ function Navbar({ config }) {
     );
 
     let navLinksAside = config.links.map((link, index) => {
-            return (
-                <Link href={link.src} className={activeClass(index + 1)} key={index + 1}>
-                    {link.name}
-                </Link>
-            );
-        });
+        return (
+            <Link href={link.src} className={activeClass(index + 1)} key={index + 1}>
+                {link.name}
+            </Link>
+        );
+    });
 
     return (
         <div className={styles.container}>
-            <div>{navHome}</div>
-            <div id={styles.linksAside}>{navLinksAside}</div>
+            <div className={styles.home}>{navHome}</div>
+            <div className={styles.linksAside}>{navLinksAside}</div>
+            <div className={styles.mobileIcon}>
+                <Link href="" onClick={toggleMobileMenu}>
+                    <Image src={MenuImage} alt="" />
+                </Link>
+            </div>
+            <div className={mobileShown ? styles.mobileMenu : styles.hidden}>
+                {navLinksAside}
+            </div>
         </div>
     );
 }
