4dfb90b4edbb8ca1dc4036bcf40e5624150d4ec6
ZeroWave022 <36341766+ZeroWave022@users.noreply.github.com>
2022-12-02 22:39:59 +0100

    feat(Warszawa): Add content and images

--- pages/cities/warszawa.js
+++ pages/cities/warszawa.js
@@ -1,7 +1,23 @@
+import Image from "next/image";
+import Link from "next/link";
+
 import Layout from "@components/Layout/Layout.js";
 import Title from "@components/Title/Title.js";
+import LandmarkInfo from "@components/LandmarkInfo/LandmarkInfo.js";
+import FullscreenImage from "@components/FullscreenImage/FullscreenImage.js";
+import DescribedImage from "@components/DescribedImage/DescribedImage.js";
 
 import getNavbarLinks from "@lib/getNavbarLinks.js";
+
+import PresidentialPalace from "@public/images/landmarks/PresidentialPalace.jpg";
+import PalaceCSLarge from "@public/images/landmarks/PalaceCS-large.jpg";
+import PalaceCSSmall from "@public/images/landmarks/PalaceCS-small.jpg";
+import RoyalCastleLarge from "@public/images/landmarks/RoyalCastle-large.jpg";
+import RoyalCastleSmall from "@public/images/landmarks/RoyalCastle-small.jpg";
+import WarsawRisingSmall from "@public/images/landmarks/WarsawRising-small.jpg";
+import WarsawRisingLarge from "@public/images/landmarks/WarsawRising-large.jpg";
+
+import styles from "@styles/CityPage.module.css";
 
 const pageConfig = {
     metadata: {
@@ -30,7 +46,55 @@ export async function getStaticProps() {
 export default function Warszawa({ metadata, navbar }) {
     return (
         <Layout metadata={metadata} navbar={navbar}>
-            <Title size="large">Warszawa</Title>
+            <LandmarkInfo>
+                <div>
+                    <Title size="large">Warszawa</Title>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis nec ligula ut lectus sodales pellentesque a sit amet lectus. Cras vestibulum imperdiet sodales. Quisque non sem mi. Vestibulum ac accumsan tortor, vel venenatis ipsum. Morbi egestas, augue sed suscipit iaculis, neque magna luctus erat, et vehicula velit nibh vel libero. Nulla facilisi. Sed sodales dolor velit, sit amet cursus lacus placerat ac. Quisque pellentesque, ligula sit amet interdum accumsan, dui augue porttitor elit, vel tincidunt est sapien vel justo. Sed et odio malesuada, pharetra urna malesuada, dignissim libero. Integer porttitor pharetra mauris. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Sed lacinia id diam id condimentum. </p>
+
+                    <p>Aenean luctus, leo ut mollis tincidunt, est libero malesuada mauris, eget facilisis elit quam in lacus. Maecenas finibus, erat a varius sagittis, diam velit egestas orci, vel posuere nunc dolor vitae velit. In fermentum quam maximus risus laoreet, vitae commodo dui auctor. In sit amet auctor magna. Donec et augue in ipsum placerat dignissim. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam elementum, orci vitae vehicula condimentum, nibh neque fringilla quam, quis varius lectus metus at lectus. In vel felis et dolor convallis efficitur iaculis non lacus.</p>
+                </div>
+                <DescribedImage src={PresidentialPalace} alt="Presidential Palace in Warsaw" description="The Presidential Palace. By wawaleszek on Pixabay" />
+            </LandmarkInfo>
+            
+            <FullscreenImage src={PalaceCSLarge} alt="The Palace of Culture and Science" title="The Palace of Culture and Science" description="By Przemysław Włodowski on Pixabay" />
+            
+            <LandmarkInfo>
+                <DescribedImage src={PalaceCSSmall} alt="The Palace of Culture and Science" description="By Adrian Grycuk on Wikimedia Commons. CC BY-SA 3.0 PL" />
+                <div>
+                    <Title size="large">The Palace of Culture and Science</Title>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis nec ligula ut lectus sodales pellentesque a sit amet lectus. Cras vestibulum imperdiet sodales. Quisque non sem mi. Vestibulum ac accumsan tortor, vel venenatis ipsum. Morbi egestas, augue sed suscipit iaculis, neque magna luctus erat, et vehicula velit nibh vel libero. Nulla facilisi. Sed sodales dolor velit, sit amet cursus lacus placerat ac. Quisque pellentesque, ligula sit amet interdum accumsan, dui augue porttitor elit, vel tincidunt est sapien vel justo. Sed et odio malesuada, pharetra urna malesuada, dignissim libero. Integer porttitor pharetra mauris. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Sed lacinia id diam id condimentum. </p>
+
+                    <p>Aenean luctus, leo ut mollis tincidunt, est libero malesuada mauris, eget facilisis elit quam in lacus. Maecenas finibus, erat a varius sagittis, diam velit egestas orci, vel posuere nunc dolor vitae velit. In fermentum quam maximus risus laoreet, vitae commodo dui auctor. In sit amet auctor magna. Donec et augue in ipsum placerat dignissim. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam elementum, orci vitae vehicula condimentum, nibh neque fringilla quam, quis varius lectus metus at lectus. In vel felis et dolor convallis efficitur iaculis non lacus.</p>
+                </div>
+            </LandmarkInfo>
+            
+            <FullscreenImage src={RoyalCastleLarge} alt="The Royal Castle" title="The Royal Castle" description="By Dariusz Staniszewski on Pixabay" />
+            
+            <LandmarkInfo>
+                <div>
+                    <Title size="large">The Royal Castle</Title>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis nec ligula ut lectus sodales pellentesque a sit amet lectus. Cras vestibulum imperdiet sodales. Quisque non sem mi. Vestibulum ac accumsan tortor, vel venenatis ipsum. Morbi egestas, augue sed suscipit iaculis, neque magna luctus erat, et vehicula velit nibh vel libero. Nulla facilisi. Sed sodales dolor velit, sit amet cursus lacus placerat ac. Quisque pellentesque, ligula sit amet interdum accumsan, dui augue porttitor elit, vel tincidunt est sapien vel justo. Sed et odio malesuada, pharetra urna malesuada, dignissim libero. Integer porttitor pharetra mauris. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Sed lacinia id diam id condimentum. </p>
+
+                    <p>Aenean luctus, leo ut mollis tincidunt, est libero malesuada mauris, eget facilisis elit quam in lacus. Maecenas finibus, erat a varius sagittis, diam velit egestas orci, vel posuere nunc dolor vitae velit. In fermentum quam maximus risus laoreet, vitae commodo dui auctor. In sit amet auctor magna. Donec et augue in ipsum placerat dignissim. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam elementum, orci vitae vehicula condimentum, nibh neque fringilla quam, quis varius lectus metus at lectus. In vel felis et dolor convallis efficitur iaculis non lacus.</p>
+                </div>
+                <DescribedImage src={RoyalCastleSmall} alt="The Royal Castle" description="By Dariusz Staniszewski on Pixabay" />
+            </LandmarkInfo>
+
+            <FullscreenImage src={WarsawRisingLarge} alt="Warsaw Rising Museum" title="Warsaw Rising Museum" description="By Adrian Grycuk on Wikimedia Commons. CC BY-SA 3.0 PL" />
+            
+            <LandmarkInfo>
+                <DescribedImage src={WarsawRisingSmall} alt="Warsaw Rising Museum" description="By Adrian Grycuk on Wikimedia Commons. CC BY-SA 3.0 PL" />
+                <div>
+                    <Title size="large">Warsaw Rising Museum</Title>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis nec ligula ut lectus sodales pellentesque a sit amet lectus. Cras vestibulum imperdiet sodales. Quisque non sem mi. Vestibulum ac accumsan tortor, vel venenatis ipsum. Morbi egestas, augue sed suscipit iaculis, neque magna luctus erat, et vehicula velit nibh vel libero. Nulla facilisi. Sed sodales dolor velit, sit amet cursus lacus placerat ac. Quisque pellentesque, ligula sit amet interdum accumsan, dui augue porttitor elit, vel tincidunt est sapien vel justo. Sed et odio malesuada, pharetra urna malesuada, dignissim libero. Integer porttitor pharetra mauris. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Sed lacinia id diam id condimentum. </p>
+
+                    <p>Aenean luctus, leo ut mollis tincidunt, est libero malesuada mauris, eget facilisis elit quam in lacus. Maecenas finibus, erat a varius sagittis, diam velit egestas orci, vel posuere nunc dolor vitae velit. In fermentum quam maximus risus laoreet, vitae commodo dui auctor. In sit amet auctor magna. Donec et augue in ipsum placerat dignissim. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam elementum, orci vitae vehicula condimentum, nibh neque fringilla quam, quis varius lectus metus at lectus. In vel felis et dolor convallis efficitur iaculis non lacus.</p>
+                </div>
+            </LandmarkInfo>
+
+            <Link className={styles.backButton} href="/cities">
+                Back to all cities
+            </Link>
         </Layout>
     );
 }
