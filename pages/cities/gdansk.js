52f16e92ddb469d6f51d1ce8a175b964d6a8225c
ZeroWave022 <36341766+ZeroWave022@users.noreply.github.com>
2022-12-04 22:25:04 +0100

    feat(Gdansk): Add content and images

--- pages/cities/gdansk.js
+++ pages/cities/gdansk.js
@@ -1,7 +1,23 @@
+import Image from "next/image";
+import Link from "next/link";
+
 import Layout from "@components/Layout/Layout.js";
 import Title from "@components/Title/Title.js";
+import LandmarkInfo from "@components/LandmarkInfo/LandmarkInfo.js";
+import FullscreenImage from "@components/FullscreenImage/FullscreenImage.js";
+import DescribedImage from "@components/DescribedImage/DescribedImage.js";
 
 import getNavbarLinks from "@lib/getNavbarLinks.js";
+
+import Ocean from "@public/images/landmarks/GdanskOcean.jpg";
+import WW2MuseumLarge from "@public/images/landmarks/WW2Museum-large.jpg";
+import WW2MuseumSmall from "@public/images/landmarks/WW2Museum-small.jpg";
+import NeptuneLarge from "@public/images/landmarks/Neptune-large.jpg";
+import NeptuneSmall from "@public/images/landmarks/Neptune-small.jpg";
+import StMaryChurchLarge from "@public/images/landmarks/StMaryChurch-large.jpg";
+import StMaryChurchSmall from "@public/images/landmarks/StMaryChurch-small.jpg";
+
+import styles from "@styles/CityPage.module.css";
 
 const pageConfig = {
     metadata: {
@@ -30,7 +46,55 @@ export async function getStaticProps() {
 export default function Gdansk({ metadata, navbar }) {
     return (
         <Layout metadata={metadata} navbar={navbar}>
-            <Title size="large">Gdańsk</Title>
+            <LandmarkInfo>
+                <div>
+                    <Title size="large">Gdańsk</Title>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis nec ligula ut lectus sodales pellentesque a sit amet lectus. Cras vestibulum imperdiet sodales. Quisque non sem mi. Vestibulum ac accumsan tortor, vel venenatis ipsum. Morbi egestas, augue sed suscipit iaculis, neque magna luctus erat, et vehicula velit nibh vel libero. Nulla facilisi. Sed sodales dolor velit, sit amet cursus lacus placerat ac. Quisque pellentesque, ligula sit amet interdum accumsan, dui augue porttitor elit, vel tincidunt est sapien vel justo. Sed et odio malesuada, pharetra urna malesuada, dignissim libero. Integer porttitor pharetra mauris. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Sed lacinia id diam id condimentum. </p>
+
+                    <p>Aenean luctus, leo ut mollis tincidunt, est libero malesuada mauris, eget facilisis elit quam in lacus. Maecenas finibus, erat a varius sagittis, diam velit egestas orci, vel posuere nunc dolor vitae velit. In fermentum quam maximus risus laoreet, vitae commodo dui auctor. In sit amet auctor magna. Donec et augue in ipsum placerat dignissim. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam elementum, orci vitae vehicula condimentum, nibh neque fringilla quam, quis varius lectus metus at lectus. In vel felis et dolor convallis efficitur iaculis non lacus.</p>
+                </div>
+                <DescribedImage src={Ocean} alt="Picture of ocean taken from the shore" description="By IdaT on Pixabay" />
+            </LandmarkInfo>
+            
+            <FullscreenImage src={WW2MuseumLarge} alt="Second World War Museum" title="World War II Museum" description="By Adam Kumiszcza on Wikimedia Commons. CC BY-SA 4.0" />
+            
+            <LandmarkInfo>
+                <DescribedImage src={WW2MuseumSmall} alt="Second World War Museum" description="By Łukasz Katlewa on Wikimedia Commons. CC BY-SA 3.0" />
+                <div>
+                    <Title size="large">World War II Museum</Title>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis nec ligula ut lectus sodales pellentesque a sit amet lectus. Cras vestibulum imperdiet sodales. Quisque non sem mi. Vestibulum ac accumsan tortor, vel venenatis ipsum. Morbi egestas, augue sed suscipit iaculis, neque magna luctus erat, et vehicula velit nibh vel libero. Nulla facilisi. Sed sodales dolor velit, sit amet cursus lacus placerat ac. Quisque pellentesque, ligula sit amet interdum accumsan, dui augue porttitor elit, vel tincidunt est sapien vel justo. Sed et odio malesuada, pharetra urna malesuada, dignissim libero. Integer porttitor pharetra mauris. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Sed lacinia id diam id condimentum. </p>
+
+                    <p>Aenean luctus, leo ut mollis tincidunt, est libero malesuada mauris, eget facilisis elit quam in lacus. Maecenas finibus, erat a varius sagittis, diam velit egestas orci, vel posuere nunc dolor vitae velit. In fermentum quam maximus risus laoreet, vitae commodo dui auctor. In sit amet auctor magna. Donec et augue in ipsum placerat dignissim. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam elementum, orci vitae vehicula condimentum, nibh neque fringilla quam, quis varius lectus metus at lectus. In vel felis et dolor convallis efficitur iaculis non lacus.</p>
+                </div>
+            </LandmarkInfo>
+            
+            <FullscreenImage src={NeptuneLarge} alt="Neptune's Fountain" title="Neptune's Fountain" description="By Janusz Kreczmański on Pixabay" />
+            
+            <LandmarkInfo>
+                <div>
+                    <Title size="large">Neptune&apos;s Fountain</Title>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis nec ligula ut lectus sodales pellentesque a sit amet lectus. Cras vestibulum imperdiet sodales. Quisque non sem mi. Vestibulum ac accumsan tortor, vel venenatis ipsum. Morbi egestas, augue sed suscipit iaculis, neque magna luctus erat, et vehicula velit nibh vel libero. Nulla facilisi. Sed sodales dolor velit, sit amet cursus lacus placerat ac. Quisque pellentesque, ligula sit amet interdum accumsan, dui augue porttitor elit, vel tincidunt est sapien vel justo. Sed et odio malesuada, pharetra urna malesuada, dignissim libero. Integer porttitor pharetra mauris. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Sed lacinia id diam id condimentum. </p>
+
+                    <p>Aenean luctus, leo ut mollis tincidunt, est libero malesuada mauris, eget facilisis elit quam in lacus. Maecenas finibus, erat a varius sagittis, diam velit egestas orci, vel posuere nunc dolor vitae velit. In fermentum quam maximus risus laoreet, vitae commodo dui auctor. In sit amet auctor magna. Donec et augue in ipsum placerat dignissim. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam elementum, orci vitae vehicula condimentum, nibh neque fringilla quam, quis varius lectus metus at lectus. In vel felis et dolor convallis efficitur iaculis non lacus.</p>
+                </div>
+                <DescribedImage src={NeptuneSmall} alt="Neptune's Fountain" description="By Robert Wroński on Pixabay" />
+            </LandmarkInfo>
+
+            <FullscreenImage src={StMaryChurchLarge} alt="Saint Mary's Church" title="Saint Mary's Church" description="By Gyddanyzc on Wikimedia Commons. CC0" />
+            
+            <LandmarkInfo>
+                <DescribedImage src={StMaryChurchSmall} alt="Saint Mary's Church" description="By Diego Delso on Wikimedia Commons. CC BY-SA 3.0" />
+                <div>
+                    <Title size="large">Saint Mary&apos;s Church</Title>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis nec ligula ut lectus sodales pellentesque a sit amet lectus. Cras vestibulum imperdiet sodales. Quisque non sem mi. Vestibulum ac accumsan tortor, vel venenatis ipsum. Morbi egestas, augue sed suscipit iaculis, neque magna luctus erat, et vehicula velit nibh vel libero. Nulla facilisi. Sed sodales dolor velit, sit amet cursus lacus placerat ac. Quisque pellentesque, ligula sit amet interdum accumsan, dui augue porttitor elit, vel tincidunt est sapien vel justo. Sed et odio malesuada, pharetra urna malesuada, dignissim libero. Integer porttitor pharetra mauris. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Sed lacinia id diam id condimentum. </p>
+
+                    <p>Aenean luctus, leo ut mollis tincidunt, est libero malesuada mauris, eget facilisis elit quam in lacus. Maecenas finibus, erat a varius sagittis, diam velit egestas orci, vel posuere nunc dolor vitae velit. In fermentum quam maximus risus laoreet, vitae commodo dui auctor. In sit amet auctor magna. Donec et augue in ipsum placerat dignissim. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam elementum, orci vitae vehicula condimentum, nibh neque fringilla quam, quis varius lectus metus at lectus. In vel felis et dolor convallis efficitur iaculis non lacus.</p>
+                </div>
+            </LandmarkInfo>
+
+            <Link className={styles.backButton} href="/cities">
+                Back to all cities
+            </Link>
         </Layout>
     );
 }
