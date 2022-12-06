d1e57d09e32efa7ad9bc12dbc87845118ae9cccf
ZeroWave022 <36341766+ZeroWave022@users.noreply.github.com>
2022-12-06 21:48:46 +0100

    feat: Add favicons

--- pages/_document.js
+++ pages/_document.js
@@ -7,6 +7,12 @@ export default function Document() {
                 <link rel="preconnect" href="https://fonts.googleapis.com" />
                 <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
                 <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@200;300;400;600&display=swap" rel="stylesheet" />
+                <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+                <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+                <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+                <link rel="manifest" href="/site.webmanifest" />
+                <meta name="msapplication-TileColor" content="#2b5797" />
+                <meta name="theme-color" content="#ffffff" />
             </Head>
             <body>
                 <Main />
