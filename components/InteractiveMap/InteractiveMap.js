c0f1bc249206483fd124b98b0a5962d14485d81f
ZeroWave022 <36341766+ZeroWave022@users.noreply.github.com>
2022-12-06 20:06:11 +0100

    feat(InteractiveMap): Add city descriptions

--- components/InteractiveMap/InteractiveMap.js
+++ components/InteractiveMap/InteractiveMap.js
@@ -1,6 +1,9 @@
 import React from "react";
 import Image from "next/image";
 import Link from "next/link";
+import { SyntheticEvent } from "react";
+
+import CityDescriptions from "@lib/CityDescriptions.js";
 
 import styles from "./InteractiveMap.module.css";
 
@@ -10,15 +13,21 @@ import styles from "./InteractiveMap.module.css";
  */
 function InteractiveMap({ src }) {
     const [highlighted, setHighlight] = React.useState(false);
+    const [currentCity, setCity] = React.useState("default");
 
-    function handleClick(e) {
+    /**
+     * @param {SyntheticEvent} e
+     * @param {string} cityName
+     */
+    function handleClick(e, cityName) {
         e.preventDefault();
 
-        // TODO: Update text in infoBox so it shows info about the city a user clicked on.
-        
-        setHighlight(true);
-
-        setTimeout(() => setHighlight(false), 2000)
+        if (CityDescriptions[cityName]) {
+            setCity(cityName);
+            
+            setHighlight(true);
+            setTimeout(() => setHighlight(false), 2000);
+        }
     }
     
     let infoBoxClass = `${styles.infoBox} ${highlighted ? styles.borderBox : ''}`;
@@ -27,20 +36,16 @@ function InteractiveMap({ src }) {
         <div className={styles.container}>
             <div className={styles.map}>
                 <Image src={src} alt="Map of Poland" />
-                <a id={styles.SZZ} href="" onClick={(e) => handleClick(e)} />
-
-                <a id={styles.GDN} href="" onClick={(e) => handleClick(e)} />
-                <a id={styles.POZ} href="" onClick={(e) => handleClick(e)} />
-                <a id={styles.WAW} href="" onClick={(e) => handleClick(e)} />
-                <a id={styles.KRK} href="" onClick={(e) => handleClick(e)} />
-                <a id={styles.WRO} href="" onClick={(e) => handleClick(e)} />
+                <a id={styles.SZZ} href="" onClick={(e) => handleClick(e, "SZZ")} />
+                <a id={styles.GDN} href="" onClick={(e) => handleClick(e, "GDN")} />
+                <a id={styles.POZ} href="" onClick={(e) => handleClick(e, "POZ")} />
+                <a id={styles.WAW} href="" onClick={(e) => handleClick(e, "WAW")} />
+                <a id={styles.KRK} href="" onClick={(e) => handleClick(e, "KRK")} />
+                <a id={styles.WRO} href="" onClick={(e) => handleClick(e, "WRO")} />
             </div>
             
-            <div className={infoBoxClass}>
-                <h1>City Name</h1>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis nec ligula ut lectus sodales pellentesque a sit amet lectus. Cras vestibulum imperdiet sodales. Quisque non sem mi. Vestibulum ac accumsan tortor, vel venenatis ipsum. Morbi egestas, augue sed suscipit iaculis, neque magna luctus erat, et vehicula velit nibh vel libero. Nulla facilisi. Sed sodales dolor velit, sit amet cursus lacus placerat ac. Quisque pellentesque, ligula sit amet interdum accumsan, dui augue porttitor elit, vel tincidunt est sapien vel justo. Sed et odio malesuada, pharetra urna malesuada, dignissim libero. Integer porttitor pharetra mauris. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Sed lacinia id diam id condimentum. </p>
-
-                <p>Aenean luctus, leo ut mollis tincidunt, est libero malesuada mauris, eget facilisis elit quam in lacus. Maecenas finibus, erat a varius sagittis, diam velit egestas orci, vel posuere nunc dolor vitae velit. In fermentum quam maximus risus laoreet, vitae commodo dui auctor. In sit amet auctor magna. Donec et augue in ipsum placerat dignissim. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam elementum, orci vitae vehicula condimentum, nibh neque fringilla quam, quis varius lectus metus at lectus. In vel felis et dolor convallis efficitur iaculis non lacus.</p>
+            <div className={infoBoxClass}> 
+                {CityDescriptions[currentCity]}
             </div>
         </div>
     );
