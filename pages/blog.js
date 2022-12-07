4d90afa8fefddb8a96b1e51ef2d1fbe41fb0f1d9
ZeroWave022 <36341766+ZeroWave022@users.noreply.github.com>
2022-12-07 20:14:12 +0100

    feat: Display blog posts on blog page

--- pages/blog.js
+++ pages/blog.js
@@ -2,11 +2,13 @@ import Layout from "@components/Layout/Layout.js";
 import Title from "@components/Title/Title.js";
 
 import getNavbarLinks from "@lib/getNavbarLinks.js";
+import { getPosts } from "@lib/blogPosts.js";
+import BlogPost from "@components/BlogPost/BlogPost";
 
 const pageConfig = {
     metadata: {
-        title: "Discover Poland: Cities",
-        description: "Discover Poland: Cities",
+        title: "Discover Poland: Blog",
+        description: "Discover Poland: Blog",
     },
     navbar: {
         active: 2,
@@ -15,6 +17,7 @@ const pageConfig = {
 
 export async function getStaticProps() {
     let navbarLinks = await getNavbarLinks();
+    let blogPosts = await getPosts();
 
     return {
         props: {
@@ -23,15 +26,21 @@ export async function getStaticProps() {
                 links: navbarLinks,
                 active: pageConfig.navbar.active,
             },
+            posts: blogPosts
         },
     };
 }
 
-export default function Cities({ metadata, navbar }) {
+export default function Blog({ metadata, navbar, posts }) {
     return (
         <Layout metadata={metadata} navbar={navbar}>
-            <Title size="large">Blog</Title>
-            <p style={{ textAlign: "center" }}>Work in progress...</p>
+            <Title size="large">Blog & Events</Title>
+            {posts.map((postData) => {
+                    return (
+                        <BlogPost post={postData} key={postData.id}></BlogPost>
+                    )
+                })
+            }
         </Layout>
     );
 }
