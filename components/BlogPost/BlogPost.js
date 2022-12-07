6186324a12dd5c942c03da1836fc5ff7eb6bbbd5
ZeroWave022 <36341766+ZeroWave022@users.noreply.github.com>
2022-12-07 20:04:35 +0100

    feat: Add BlogPost component

---
+++ components/BlogPost/BlogPost.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+import Title from "@components/Title/Title.js";
+
+import styles from "./BlogPost.module.css";
+import TextLink from "@components/TextLink/TextLink";
+
+function BlogPost({ post }) {
+    return (
+        <div className={styles.container}>
+            <Title size="medium">{post.title}</Title>
+            <div className={styles.date} dangerouslySetInnerHTML={{ __html: post.date }} />
+            <div className={styles.content} dangerouslySetInnerHTML={{ __html: post.content }} />
+            {
+                // If the post has a source, add it as a link to the bottom of post.
+                post.sourceLink ? (
+                    <div>
+                        <span>Source: </span>
+                        <TextLink href={post.sourceLink}>{post.sourceName}</TextLink>
+                    </div>
+                ) : null
+            }
+        </div>
+    );
+}
+
+export default BlogPost;
