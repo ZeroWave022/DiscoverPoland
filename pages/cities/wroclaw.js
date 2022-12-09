ea6d841f722b3c262be5f5afbfb21628f30aa417
ZeroWave_ <36341766+ZeroWave022@users.noreply.github.com>
2022-12-09 09:56:20 +0100

    feat(Wroclaw): Add text

--- pages/cities/wroclaw.js
+++ pages/cities/wroclaw.js
@@ -50,9 +50,14 @@ export default function Wroclaw({ metadata, navbar }) {
             <LandmarkInfo>
                 <div className={LIstyles.text}>
                     <Title size="large">Wrocław</Title>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis nec ligula ut lectus sodales pellentesque a sit amet lectus. Cras vestibulum imperdiet sodales. Quisque non sem mi. Vestibulum ac accumsan tortor, vel venenatis ipsum. Morbi egestas, augue sed suscipit iaculis, neque magna luctus erat, et vehicula velit nibh vel libero. Nulla facilisi. Sed sodales dolor velit, sit amet cursus lacus placerat ac. Quisque pellentesque, ligula sit amet interdum accumsan, dui augue porttitor elit, vel tincidunt est sapien vel justo. Sed et odio malesuada, pharetra urna malesuada, dignissim libero. Integer porttitor pharetra mauris. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Sed lacinia id diam id condimentum. </p>
-
-                    <p>Aenean luctus, leo ut mollis tincidunt, est libero malesuada mauris, eget facilisis elit quam in lacus. Maecenas finibus, erat a varius sagittis, diam velit egestas orci, vel posuere nunc dolor vitae velit. In fermentum quam maximus risus laoreet, vitae commodo dui auctor. In sit amet auctor magna. Donec et augue in ipsum placerat dignissim. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam elementum, orci vitae vehicula condimentum, nibh neque fringilla quam, quis varius lectus metus at lectus. In vel felis et dolor convallis efficitur iaculis non lacus.</p>
+                    <p>
+                        Wrocław lies of the Oder river, and has a population of over 670 000 inhabitants.
+                        The city has experienced a lot of historical events in the past which formed what it is today.
+                    </p>
+                    <p>
+                        The Wrocław Zoo, established in 1865, is the oldest zoological garden in Poland, and is a popular tourist attraction.
+                        The Racławice Panorama is also a popular point of interest, depicting the Battle of Racławice, fought in 1794.
+                    </p>
                 </div>
                 <DescribedImage src={Landscape} alt="Wroclaw Landscape" description="Wrocław. By Dariusz Sankowski on Pixabay" />
             </LandmarkInfo>
@@ -63,9 +68,15 @@ export default function Wroclaw({ metadata, navbar }) {
                 <DescribedImage src={RaclawiceSmall} alt="Raclawice Panorama" description="By Andreas Lischka on Pixabay" />
                 <div className={LIstyles.text}>
                     <Title size="large">Racławice Panorama</Title>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis nec ligula ut lectus sodales pellentesque a sit amet lectus. Cras vestibulum imperdiet sodales. Quisque non sem mi. Vestibulum ac accumsan tortor, vel venenatis ipsum. Morbi egestas, augue sed suscipit iaculis, neque magna luctus erat, et vehicula velit nibh vel libero. Nulla facilisi. Sed sodales dolor velit, sit amet cursus lacus placerat ac. Quisque pellentesque, ligula sit amet interdum accumsan, dui augue porttitor elit, vel tincidunt est sapien vel justo. Sed et odio malesuada, pharetra urna malesuada, dignissim libero. Integer porttitor pharetra mauris. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Sed lacinia id diam id condimentum. </p>
-
-                    <p>Aenean luctus, leo ut mollis tincidunt, est libero malesuada mauris, eget facilisis elit quam in lacus. Maecenas finibus, erat a varius sagittis, diam velit egestas orci, vel posuere nunc dolor vitae velit. In fermentum quam maximus risus laoreet, vitae commodo dui auctor. In sit amet auctor magna. Donec et augue in ipsum placerat dignissim. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam elementum, orci vitae vehicula condimentum, nibh neque fringilla quam, quis varius lectus metus at lectus. In vel felis et dolor convallis efficitur iaculis non lacus.</p>
+                    <p>
+                        This Panorama, with the viewer in the middle, depicts the Battle of Racławice, fought in 1794.
+                        The battle was fought during the Kościuszko Uprising, which was led by Tadeusz Kościuszko.
+                        A special kind of perspective is used, and additional effects like lightning and artificial terrain create a feeling of being in the middle of the battle.
+                    </p>
+                    <p>
+                        This unique experience is fantastic for fans of history, and those who would like to learn more about Polish history.
+                        This paining is also one of the few preserved relics of 19th century mass culture in Poland.
+                    </p>
                 </div>
             </LandmarkInfo>
             
@@ -74,9 +85,14 @@ export default function Wroclaw({ metadata, navbar }) {
             <LandmarkInfo>
                 <div className={LIstyles.text}>
                     <Title size="large">Wrocław Zoo</Title>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis nec ligula ut lectus sodales pellentesque a sit amet lectus. Cras vestibulum imperdiet sodales. Quisque non sem mi. Vestibulum ac accumsan tortor, vel venenatis ipsum. Morbi egestas, augue sed suscipit iaculis, neque magna luctus erat, et vehicula velit nibh vel libero. Nulla facilisi. Sed sodales dolor velit, sit amet cursus lacus placerat ac. Quisque pellentesque, ligula sit amet interdum accumsan, dui augue porttitor elit, vel tincidunt est sapien vel justo. Sed et odio malesuada, pharetra urna malesuada, dignissim libero. Integer porttitor pharetra mauris. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Sed lacinia id diam id condimentum. </p>
-
-                    <p>Aenean luctus, leo ut mollis tincidunt, est libero malesuada mauris, eget facilisis elit quam in lacus. Maecenas finibus, erat a varius sagittis, diam velit egestas orci, vel posuere nunc dolor vitae velit. In fermentum quam maximus risus laoreet, vitae commodo dui auctor. In sit amet auctor magna. Donec et augue in ipsum placerat dignissim. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam elementum, orci vitae vehicula condimentum, nibh neque fringilla quam, quis varius lectus metus at lectus. In vel felis et dolor convallis efficitur iaculis non lacus.</p>
+                    <p>
+                        The Wrocław Zoological Garden was opened in 1865 and is the oldest zoo in Poland.
+                        The zoo covers 33 hectares and is home to approximately 10 500 animals and over 1 100 species.
+                    </p>
+                    <p>
+                        The zoo is definitively worthwhile, as stats show: This is the most visited zoo in Poland, and the fifth most visited zoo in Europe.
+                        It is also a member of the European Association of Zoos and Aquaria and the World Association of Zoos and Aquariums.
+                    </p>
                 </div>
                 <DescribedImage src={ZooSmall} alt="Wroclaw Zoo" description="By Rafał Chudoba on Pixabay" />
             </LandmarkInfo>
@@ -87,9 +103,15 @@ export default function Wroclaw({ metadata, navbar }) {
                 <DescribedImage src={CHSmall} alt="Centennial Hall" description="By Łukasz Tekieli on Pixabay" />
                 <div className={LIstyles.text}>
                     <Title size="large">Centennial Hall</Title>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis nec ligula ut lectus sodales pellentesque a sit amet lectus. Cras vestibulum imperdiet sodales. Quisque non sem mi. Vestibulum ac accumsan tortor, vel venenatis ipsum. Morbi egestas, augue sed suscipit iaculis, neque magna luctus erat, et vehicula velit nibh vel libero. Nulla facilisi. Sed sodales dolor velit, sit amet cursus lacus placerat ac. Quisque pellentesque, ligula sit amet interdum accumsan, dui augue porttitor elit, vel tincidunt est sapien vel justo. Sed et odio malesuada, pharetra urna malesuada, dignissim libero. Integer porttitor pharetra mauris. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Sed lacinia id diam id condimentum. </p>
-
-                    <p>Aenean luctus, leo ut mollis tincidunt, est libero malesuada mauris, eget facilisis elit quam in lacus. Maecenas finibus, erat a varius sagittis, diam velit egestas orci, vel posuere nunc dolor vitae velit. In fermentum quam maximus risus laoreet, vitae commodo dui auctor. In sit amet auctor magna. Donec et augue in ipsum placerat dignissim. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam elementum, orci vitae vehicula condimentum, nibh neque fringilla quam, quis varius lectus metus at lectus. In vel felis et dolor convallis efficitur iaculis non lacus.</p>
+                    <p>
+                        The Hall was built by the architect Max Berg in 1911-1913.
+                        The building was designated to host exhibitions, concerts, theatrical and opera performances and sporting events.
+                    </p>
+                    <p>
+                        Today, the Centennial Hall is still used for these purposes.
+                        Concerts, business summits, and sporting events are often hosted here, as part of Wrocław&apos;s city life.
+                        It was also listed as a UNESCO World Heritage site in 2006.
+                    </p>
                 </div>
             </LandmarkInfo>
 
