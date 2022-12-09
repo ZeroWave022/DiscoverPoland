294af9651628464838a58f45d23d3e51df10dd19
ZeroWave_ <36341766+ZeroWave022@users.noreply.github.com>
2022-12-09 12:53:07 +0100

    feat(Gdansk): Add text

--- pages/cities/gdansk.js
+++ pages/cities/gdansk.js
@@ -50,9 +50,15 @@ export default function Gdansk({ metadata, navbar }) {
             <LandmarkInfo>
                 <div className={LIstyles.text}>
                     <Title size="large">Gdańsk</Title>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis nec ligula ut lectus sodales pellentesque a sit amet lectus. Cras vestibulum imperdiet sodales. Quisque non sem mi. Vestibulum ac accumsan tortor, vel venenatis ipsum. Morbi egestas, augue sed suscipit iaculis, neque magna luctus erat, et vehicula velit nibh vel libero. Nulla facilisi. Sed sodales dolor velit, sit amet cursus lacus placerat ac. Quisque pellentesque, ligula sit amet interdum accumsan, dui augue porttitor elit, vel tincidunt est sapien vel justo. Sed et odio malesuada, pharetra urna malesuada, dignissim libero. Integer porttitor pharetra mauris. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Sed lacinia id diam id condimentum. </p>
-
-                    <p>Aenean luctus, leo ut mollis tincidunt, est libero malesuada mauris, eget facilisis elit quam in lacus. Maecenas finibus, erat a varius sagittis, diam velit egestas orci, vel posuere nunc dolor vitae velit. In fermentum quam maximus risus laoreet, vitae commodo dui auctor. In sit amet auctor magna. Donec et augue in ipsum placerat dignissim. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam elementum, orci vitae vehicula condimentum, nibh neque fringilla quam, quis varius lectus metus at lectus. In vel felis et dolor convallis efficitur iaculis non lacus.</p>
+                    <p>
+                        Gdańsk, with its population of 470 000, is a city with a lot of surprises for tourists!
+                        Featuring fantastic architecture like St. Mary&apos;s Church, a Ferris wheel, Neptune&apos;s Fountain and much more to discover.
+                    </p>
+                    <p>
+                        Those interested in history will also be pleased to hear Gdańsk has a Second World War museum. The Battle of Westerplatte, a peninsula in Gdańsk, was the first battle of WW2.
+                        The city also hosts a Museum of Technology and Transport.
+                        Gdańsk has scored very high on ranking for quality of life, safety and living standards worldwide, and the city is very welcoming to tourists.
+                    </p>
                 </div>
                 <DescribedImage src={Ocean} alt="Picture of ocean taken from the shore" description="By IdaT on Pixabay" />
             </LandmarkInfo>
@@ -63,9 +69,15 @@ export default function Gdansk({ metadata, navbar }) {
                 <DescribedImage src={WW2MuseumSmall} alt="Second World War Museum" description="By Łukasz Katlewa on Wikimedia Commons. CC BY-SA 3.0" />
                 <div className={LIstyles.text}>
                     <Title size="large">World War II Museum</Title>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis nec ligula ut lectus sodales pellentesque a sit amet lectus. Cras vestibulum imperdiet sodales. Quisque non sem mi. Vestibulum ac accumsan tortor, vel venenatis ipsum. Morbi egestas, augue sed suscipit iaculis, neque magna luctus erat, et vehicula velit nibh vel libero. Nulla facilisi. Sed sodales dolor velit, sit amet cursus lacus placerat ac. Quisque pellentesque, ligula sit amet interdum accumsan, dui augue porttitor elit, vel tincidunt est sapien vel justo. Sed et odio malesuada, pharetra urna malesuada, dignissim libero. Integer porttitor pharetra mauris. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Sed lacinia id diam id condimentum. </p>
-
-                    <p>Aenean luctus, leo ut mollis tincidunt, est libero malesuada mauris, eget facilisis elit quam in lacus. Maecenas finibus, erat a varius sagittis, diam velit egestas orci, vel posuere nunc dolor vitae velit. In fermentum quam maximus risus laoreet, vitae commodo dui auctor. In sit amet auctor magna. Donec et augue in ipsum placerat dignissim. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam elementum, orci vitae vehicula condimentum, nibh neque fringilla quam, quis varius lectus metus at lectus. In vel felis et dolor convallis efficitur iaculis non lacus.</p>
+                    <p>
+                        This museum was devoted to the Second World War, as Gdańsk was a city involved in the war.
+                        The Battle of Westerplatte, a peninsula in Gdańsk, was the first battle of WW2.
+                    </p>
+                    <p>
+                        Its exhibitions opened in 2017, and today it hosts military vehicles, personal items and other objects of significance.
+                        The architectural team responsible for the building of the museum, named Kwadrat (from Polish: The Square), won an architectural competiton for their work.
+                        In 2017, the museum had over 417 000 visitors.
+                    </p>
                 </div>
             </LandmarkInfo>
             
@@ -74,9 +86,14 @@ export default function Gdansk({ metadata, navbar }) {
             <LandmarkInfo>
                 <div className={LIstyles.text}>
                     <Title size="large">Neptune&apos;s Fountain</Title>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis nec ligula ut lectus sodales pellentesque a sit amet lectus. Cras vestibulum imperdiet sodales. Quisque non sem mi. Vestibulum ac accumsan tortor, vel venenatis ipsum. Morbi egestas, augue sed suscipit iaculis, neque magna luctus erat, et vehicula velit nibh vel libero. Nulla facilisi. Sed sodales dolor velit, sit amet cursus lacus placerat ac. Quisque pellentesque, ligula sit amet interdum accumsan, dui augue porttitor elit, vel tincidunt est sapien vel justo. Sed et odio malesuada, pharetra urna malesuada, dignissim libero. Integer porttitor pharetra mauris. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Sed lacinia id diam id condimentum. </p>
-
-                    <p>Aenean luctus, leo ut mollis tincidunt, est libero malesuada mauris, eget facilisis elit quam in lacus. Maecenas finibus, erat a varius sagittis, diam velit egestas orci, vel posuere nunc dolor vitae velit. In fermentum quam maximus risus laoreet, vitae commodo dui auctor. In sit amet auctor magna. Donec et augue in ipsum placerat dignissim. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam elementum, orci vitae vehicula condimentum, nibh neque fringilla quam, quis varius lectus metus at lectus. In vel felis et dolor convallis efficitur iaculis non lacus.</p>
+                    <p>
+                        Neptune&apos;s fountain is one of the most important landmarks of the city.
+                        It is located at the Długi Targ (from Polish: Long Market), and it was constructed in 1606-1633.
+                    </p>
+                    <p>
+                        The architectural style of Neptune&apos;s is femish ammnerism and rococo.
+                        While you&apos;re visiting this fine piece of architecture, you will also be in the Long Market, which is home to many restaurants and other services.
+                    </p>
                 </div>
                 <DescribedImage src={NeptuneSmall} alt="Neptune's Fountain" description="By Robert Wroński on Pixabay" />
             </LandmarkInfo>
@@ -87,9 +104,14 @@ export default function Gdansk({ metadata, navbar }) {
                 <DescribedImage src={StMaryChurchSmall} alt="Saint Mary's Church" description="By Diego Delso on Wikimedia Commons. CC BY-SA 3.0" />
                 <div className={LIstyles.text}>
                     <Title size="large">Saint Mary&apos;s Church</Title>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis nec ligula ut lectus sodales pellentesque a sit amet lectus. Cras vestibulum imperdiet sodales. Quisque non sem mi. Vestibulum ac accumsan tortor, vel venenatis ipsum. Morbi egestas, augue sed suscipit iaculis, neque magna luctus erat, et vehicula velit nibh vel libero. Nulla facilisi. Sed sodales dolor velit, sit amet cursus lacus placerat ac. Quisque pellentesque, ligula sit amet interdum accumsan, dui augue porttitor elit, vel tincidunt est sapien vel justo. Sed et odio malesuada, pharetra urna malesuada, dignissim libero. Integer porttitor pharetra mauris. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Sed lacinia id diam id condimentum. </p>
-
-                    <p>Aenean luctus, leo ut mollis tincidunt, est libero malesuada mauris, eget facilisis elit quam in lacus. Maecenas finibus, erat a varius sagittis, diam velit egestas orci, vel posuere nunc dolor vitae velit. In fermentum quam maximus risus laoreet, vitae commodo dui auctor. In sit amet auctor magna. Donec et augue in ipsum placerat dignissim. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam elementum, orci vitae vehicula condimentum, nibh neque fringilla quam, quis varius lectus metus at lectus. In vel felis et dolor convallis efficitur iaculis non lacus.</p>
+                    <p>
+                        This church is one of the three biggest brick churches in the world.
+                        It has been completed in 1502, and has 1 bell tower as well as 5 smaller spires.
+                    </p>
+                    <p>
+                        The church also has multiple works of religious art.
+                        It is also a dominant piece of Gdańsk&apos;s panorama, and it is worth visiting to learn more about the religious aspects of Poland.
+                    </p>
                 </div>
             </LandmarkInfo>
 
