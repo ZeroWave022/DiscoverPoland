d9bcb22ab5c498af2455f8124ded5cf4c87352b3
ZeroWave022 <36341766+ZeroWave022@users.noreply.github.com>
2022-12-09 17:24:34 +0100

    feat(About): Add about page

--- pages/about.js
+++ pages/about.js
@@ -1,7 +1,10 @@
 import Layout from "@components/Layout/Layout.js";
+import TextLink from "@components/TextLink/TextLink";
 import Title from "@components/Title/Title.js";
 
 import getNavbarLinks from "@lib/getNavbarLinks.js";
+
+import styles from "@styles/About.module.css";
 
 const pageConfig = {
     metadata: {
@@ -30,8 +33,22 @@ export async function getStaticProps() {
 export default function Cities({ metadata, navbar }) {
     return (
         <Layout metadata={metadata} navbar={navbar}>
-            <Title size="large">About</Title>
-            <p style={{ textAlign: "center" }}>Work in progress...</p>
+            <Title size="large">About Discover Poland</Title>
+            <p className={styles.infoParagraph}>
+                The project was made to inspire and encourage more tourists to discover Poland.
+                This country in Central Europe has multiple cities, natural areas and mountains which are all worth visiting.
+            </p>
+            <Title size="medium">Contributing</Title>
+            <p className={styles.infoParagraph}>
+                Discover Poland is available on <TextLink href="https://github.com/ZeroWave022/DiscoverPoland" target="_blank">GitHub</TextLink>.
+                To contribute, or inform about an issue, please refer to the repository.
+                Any contributions and reported issues are appreciated!
+            </p>
+            <Title size="medium">Sources</Title>
+            <p className={styles.infoParagraph}>
+                Information about cities and tourist attractions have been retrieved from Wikipedia - thank you to all the authors.
+                Photos have been retriveed from Pixabay and Wikimedia Commons. Attribution of authors is done on most photos on the website.
+            </p>
         </Layout>
     );
 }
