d5cf041714f493469a11f358e00b1cdfa14c2bf4
ZeroWave022 <36341766+ZeroWave022@users.noreply.github.com>
2022-12-09 17:53:30 +0100

    feat(TextLink): Better hover animation

--- components/TextLink/TextLink.js
+++ components/TextLink/TextLink.js
@@ -3,40 +3,13 @@ import Link from "next/link";
 
 import styles from "./TextLink.module.css";
 
-const shadowClasses = {
-    medium: styles.mediumShadow,
-    large: styles.largeShadow
-}
-
-const colorClasses = {
-    light: styles.lightLink,
-    dark: styles.darkLink
-}
-
-function TextLink({ href, target, shadowSize, color, children }) {
+function TextLink({ href, target, children }) {
     let attributes = {
         className: styles.textLink,
         href: href,
         target: target,
         rel: "noreferrer",
     };
-
-    let chosenShadow = shadowClasses[shadowSize];
-    let chosenColor = colorClasses[color];
-
-    // If a shadow is chosen, and it's valid, use that.
-    // Otherwise, use the default (which is large).
-    if (chosenShadow) {
-        attributes.className += ` ${chosenShadow}`;
-    } else {
-        attributes.className += ` ${shadowClasses.large}`;
-    }
-
-    if (chosenColor) {
-        attributes.className += ` ${chosenColor}`;
-    } else {
-        attributes.className += ` ${colorClasses.light}`;
-    }
 
     return React.createElement(Link, attributes, children);
 }
